e43911e2d2c14331320221afe399121bf1d6ce14
Sylvain Leroy <sleroy0@gmail.com>
2020-12-12 22:28:40 +0100

    fix(logger): fixed doubled output

--- bin/logger.js
+++ bin/logger.js
@@ -30,13 +30,10 @@ exports.logger = winston.createLogger({
     ),
     defaultMeta: { service: 'byoskill-code-generator' },
     transports: [
-        //
-        // - Write all logs with level `error` and below to `error.log`
-        // - Write all logs with level `info` and below to `combined.log`
-        //
-        new winston.transports.Console({
-            format: winston.format.simple()
-        })
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //        
     ],
 });
 //
