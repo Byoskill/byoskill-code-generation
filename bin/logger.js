615495e60870f47aa06a41a74cf6364b78513b5d
Sylvain Leroy <sleroy0@gmail.com>
2020-12-13 16:49:50 +0100

    feat(codegeneration) addingfile logger

--- bin/logger.js
+++ bin/logger.js
@@ -30,10 +30,11 @@ exports.logger = winston.createLogger({
     ),
     defaultMeta: { service: 'byoskill-code-generator' },
     transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //        
+        //
+        // - Write all logs with level `error` and below to `error.log`
+        // - Write all logs with level `info` and below to `combined.log`
+        //        
+        new winston.transports.File({ filename: 'codegeneration.log' }),
     ],
 });
 //
--- src/logger.ts
+++ src/logger.ts
@@ -13,6 +13,7 @@ export const logger = winston.createLogger({
         // - Write all logs with level `error` and below to `error.log`
         // - Write all logs with level `info` and below to `combined.log`
         //        
+        new winston.transports.File({ filename: 'codegeneration.log' }),
     ],
 });
 
@@ -32,5 +33,3 @@ if (process.env.NODE_ENV !== 'production') {
 }
 exports.logger.info('Logger is configured')
 // # sourceMappingURL=logger.js.map
-
-
